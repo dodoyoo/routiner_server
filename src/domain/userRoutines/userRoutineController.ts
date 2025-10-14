181f0fbd95eb60093bb0064a215cd488562923f9
dodoyoo <dong15903@daum.net>
2025-10-14 15:57:27 +0900

    사용자 선택 루틴 에러코드 수정

--- src/domain/userRoutines/userRoutineController.ts
+++ src/domain/userRoutines/userRoutineController.ts
@@ -18,15 +18,6 @@ export class UserRoutineController {
     try {
       const { user_id, routine_id, start_date, end_date } = req.body;
 
-      if (!user_id) {
-        const err = new ValidationError(
-          '사용자를 찾을 수 없습니다.',
-          'NOT FOUND',
-          404
-        );
-        return reportErrorMessage(err, res);
-      }
-
       if (!user_id || !routine_id) {
         const err = new PropertyRequiredError(
           'user_id와 routine_id의 값이 없습니다.'
@@ -45,10 +36,10 @@ export class UserRoutineController {
         data: userRoutine,
       });
     } catch (error: any) {
-      console.error('루틴 선택 중 오류:', error);
+      console.error(error);
 
-      if (error.message === '이미 선택한 루틴입니다.') {
-        return res.status(404).json({ message: error.message });
+      if (error.statusCode) {
+        return res.status(error.statusCode).json({ message: error.message });
       }
       return res.status(500).json({ message: '루틴 선택에 실패했습니다.' });
     }
