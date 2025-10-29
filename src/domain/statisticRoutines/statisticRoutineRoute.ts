2d7914dc75a7763293e775b670ae736a12dc0ec4
dodoyoo <dong15903@daum.net>
2025-10-29 16:52:07 +0900

    feat: 주간 통계 조회 API

--- src/domain/statisticRoutines/statisticRoutineRoute.ts
+++ src/domain/statisticRoutines/statisticRoutineRoute.ts
@@ -4,7 +4,7 @@ import { StaticRoutineController } from './statisticRoutineController';
 const router = Router();
 const staticRoutineController = new StaticRoutineController();
 
-router.get('/api/:user_id/week', (req, res) =>
+router.get('/api/week/:user_id', (req, res) =>
   staticRoutineController.getWeeklyStats(req, res)
 );
 
