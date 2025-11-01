13949ee54b000f104d6127b053e82e6bc6f59ea5
dodoyoo <dong15903@daum.net>
2025-11-01 15:19:10 +0900

    feat: couponEntity

---
+++ src/domain/rewards/couponEntity.ts
@@ -0,0 +1,53 @@
+import 'reflect-metadata';
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+import { User } from '../users/userEntity';
+import { UserRoutines } from '../userRoutines/userRoutineEntity';
+
+export type CouponStatus = 'issued' | 'redeemed' | 'expired';
+
+@Entity('coupons')
+@Unique('uniq_coupon_period', [
+  'user_id',
+  'user_routine_id',
+  'period_start',
+  'period_end',
+])
+export class Coupon {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'integer', nullable: false })
+  user_id!: number;
+
+  @Column({ type: 'integer', nullable: false })
+  user_routine_id!: number;
+
+  @Column({ type: 'date', nullable: false })
+  period_start!: Date;
+
+  @Column({ type: 'date', nullable: false })
+  period_end!: Date;
+
+  @Column({ type: 'varchar', length: 20, nullable: false, default: 'issued' })
+  status!: CouponStatus;
+
+  @Column({ type: 'timestamp', nullable: true })
+  redeemed_at!: Date | null;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  issued_at!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updated_at!: Date;
+}
