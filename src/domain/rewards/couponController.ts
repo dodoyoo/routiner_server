330a551c5070846b142e3e1dab75d19e44d08cc7
dodoyoo <dong15903@daum.net>
2025-11-06 14:42:11 +0900

    쿠폰 발급repository

---
+++ src/domain/rewards/couponController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import { CouponRepository } from './couponRepository';
+import { PropertyRequiredError } from '../../utils/customError';
+import { reportErrorMessage } from '../../utils/errorHandling';
+
+export class CouponController {}
