65aa2c897bd472f6880bc2121a9a7cec5f558580
dodoyoo <dong15903@daum.net>
2025-11-19 17:12:01 +0900

    feat: middleware & token 설정

--- src/domain/routines/routineRoute.ts
+++ src/domain/routines/routineRoute.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { RoutineController } from './routineController';
+import { authMiddleware } from '../../utils/jwtAuth';
 
 const router = Router();
 const routineController = new RoutineController();
 
-router.get('/api/routines', (req, res) =>
+router.get('/api/routines', authMiddleware, (req, res) =>
   routineController.allRoutines(req, res)
 );
 
-router.get('/api/routines/:category', (req, res) =>
+router.get('/api/routines/:category', authMiddleware, (req, res) =>
   routineController.routinesByCategory(req, res)
 );
 
---
+++ src/utils/jwt.d.ts
@@ -0,0 +1,6 @@
+import { JwtPayload } from 'jsonwebtoken';
+
+export interface JwtUserPayload extends JwtPayload {
+  userId: number;
+  provider: 'google' | 'kakao';
+}
