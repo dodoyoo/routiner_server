78260ec6b283bcb3b58d615f7d44f4d6b2265c28
dodoyoo <dong15903@daum.net>
2025-11-25 15:03:24 +0900

    add: authMiddleware in userRoutine

--- src/domain/userRoutines/userRoutineRoute.ts
+++ src/domain/userRoutines/userRoutineRoute.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 import { UserRoutineController } from './userRoutineController';
+import { authMiddleware } from '../../utils/jwtAuth';
 
 const router = Router();
 const userRoutineController = new UserRoutineController();
 
-router.post('/api/user-routines', (req, res) =>
+router.post('/api/user-routines', authMiddleware, (req, res) =>
   userRoutineController.createUserRoutines(req, res)
 );
 
-router.get('/api/user-routines/:user_id', (req, res) =>
+router.get('/api/user-routines/:user_id', authMiddleware, (req, res) =>
   userRoutineController.getUserRoutines(req, res)
 );
 
-router.delete('/api/user-routines/:user_id', (req, res) =>
+router.delete('/api/user-routines/:user_id', authMiddleware, (req, res) =>
   userRoutineController.deleteRoutines(req, res)
 );
 
